90d16e649923188aa2a1114dc0a46f566d798a2f
pratiksagare <79927151+pratiksagare@users.noreply.github.com>
2023-10-08 17:13:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
-import { useEffect, useState, createContext } from 'react';
+import { useEffect, useState, createContext,useLayoutEffect } from 'react';
 // import './App.css';
 import axios from "axios";
 import { Banner } from './Banner';
 import { Dish } from './Dish';
 import { Navbar } from './Navbar';
 import { Loader } from './Loader'
-// import './style.scss';
+// import './style.scss'
+// import '../public/style.scss'
 import { SkeletonBanner } from './SkeletonBanner';
 export const dataContext = createContext();
 function App() {
@@ -14,9 +15,27 @@ function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [showSkeleton, setShowSkeleton] = useState(true);
+  const [isStylesheetLoaded, setIsStylesheetLoaded] = useState(false);
 
+  useLayoutEffect(() => {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = '/public/style.scss';
+
+    linkElement.onload = () => {
+      setIsStylesheetLoaded(true);
+    };
+
+    // Append the link element to the head of the document
+    document.head.appendChild(linkElement);
+
+    // Cleanup: remove the dynamically added link element when the component unmounts
+    return () => {
+      document.head.removeChild(linkElement);
+    };
+  }, []);
   useEffect(() => {
-    
+
     const getData = async () => {
       try {
         await axios.get(`./data.json`).then(response => {
@@ -28,38 +47,34 @@ function App() {
       }
       finally {
 
-        // setTimeout(() => {
-        //   setLoading(false);
-        //   // setShowData(true);
-        // }, 2000);
         setTimeout(() => {
           setLoading(false);
           // setShowData(true);
         }, 2000);
         // setLoading(false);
-        
+
         setTimeout(() => {
           // setLoading(false);
-          
-          
+
+
           setShowSkeleton(false);
         }, 4000);
         // Set loading to false regardless of success or failure
 
       }
     };
-
-    getData();
-  }, []);
+    if (isStylesheetLoaded) {
+      getData();
+    }
+  }, [isStylesheetLoaded]);
 
   console.log(data, "data");
   return (
     <div className="App">
       <Navbar />
-      {loading ? ((
+      {/* {loading ? ((
         <div className='Loading'>
           <Loader />
-
         </div>
       )) : ((
         showSkeleton ? ((<SkeletonBanner />)) : (
@@ -73,7 +88,24 @@ function App() {
           </div>
         )
       )
-      )}
+      )} */}
+      {
+        (loading || showSkeleton) ? (
+          <div className='Loading'>
+            {loading && <Loader/>}
+            {showSkeleton && <SkeletonBanner/>}
+          </div>
+        ):(
+          <div className='marginClass'>
+
+            <dataContext.Provider value={{ data, setData }}>
+              <Banner />
+              <Dish />
+            </dataContext.Provider>
+
+          </div>
+        )
+      }
     </div>
   );
 }

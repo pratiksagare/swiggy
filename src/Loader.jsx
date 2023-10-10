411ba7c42bf407ba357eac9bb5378092006b0357
pratiksagare <79927151+pratiksagare@users.noreply.github.com>
2023-10-11 01:11:30 +0530

    Add files via upload

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,6 +1,11 @@
+import LoadImg from './icecream.png';
 export const Loader=()=>{
     return(
         <div className="Loader">
+            <div className="loading-circle">
+            <div class="loader-circle"></div>
+                <img src={LoadImg} alt="loading"/>
+            </div>
             <span className="Loader-msg">Looking for great food near you ...</span>
         </div>
     )
